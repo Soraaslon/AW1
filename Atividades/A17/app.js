d25a2bd2517b76cdf0c00835d6a51aeda1bb20f7
José Claudino <69358837+Soraaslon@users.noreply.github.com>
2021-10-26 00:13:25 -0300

    Update app.js

--- Atividades/A17/app.js
+++ Atividades/A17/app.js
@@ -38,7 +38,20 @@ function TestaCPF(strCPF) {
   var Resto
   Soma = 0
   var i
-  if (strCPF == '00000000000') return false
+  if (
+    strCPF.length != 11 ||
+    strCPF == '00000000000' ||
+    strCPF == '11111111111' ||
+    strCPF == '22222222222' ||
+    strCPF == '33333333333' ||
+    strCPF == '44444444444' ||
+    strCPF == '55555555555' ||
+    strCPF == '66666666666' ||
+    strCPF == '77777777777' ||
+    strCPF == '88888888888' ||
+    strCPF == '99999999999'
+  )
+    return false
 
   for (i = 1; i <= 9; i++)
     Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i)
